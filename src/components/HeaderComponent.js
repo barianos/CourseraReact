9b012cb444b01e0e871e21aa0989c98200026898
Anastasios Barianos <barianos@outlook.com>
2021-02-12 12:28:54 +0200

    uncontrolled forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,17 +6,21 @@ import {
   NavbarToggler,
   Collapse,
   NavItem,
-  Jumbotron,
+  Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.toggleNav = this.toggleNav.bind(this);
+    
     this.state = {
       isNavOpen: false,
+      isModalOpen:false
     };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
@@ -24,6 +28,19 @@ class Header extends Component {
       isNavOpen: !this.state.isNavOpen,
     });
   }
+
+  toggleModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  
+  handleLogin(event){
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: "+this.password.value + " Remeber: "+ this.remember.checked);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
@@ -63,13 +80,20 @@ class Header extends Component {
                     </NavLink>
                   </NavItem>
                 </Nav>
+                <Nav className='ml-auto' navbar>
+                  <NavItem>
+                    <Button outline onClick={this.toggleModal}>
+                      <span className="fa fa-sign-in fa-lg"></span>Login
+                    </Button>
+                  </NavItem>
+                </Nav>
               </Collapse>
             </div>
           </Navbar>
           <Jumbotron>
             <div className="container">
               <div className="row row-header">
-                <div clasName="col-12 col-sm-6">
+                <div className="col-12 col-sm-6">
                   <h1>Ristorante Con Fusion</h1>
                   <p>
                     We take inspiration from from the Worlds best cuisines and
@@ -80,6 +104,28 @@ class Header extends Component {
               </div>
             </div>
           </Jumbotron>
+          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+            <ModalBody>
+              <Form onSubmit={this.handleLogin}>
+                <FormGroup>
+                  <Label htmlFor="username">Username</Label>
+                  <Input type="text" id ="username" name="username" innerRef={(input) => this.username = input}></Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="password">Username</Label>
+                  <Input type="password" id ="password" name="password" innerRef={(input) => this.password = input}></Input>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input} />
+                    Remember me
+                  </Label>
+                </FormGroup>
+                <Button type="submit" value="submit" color="primary">Login</Button>
+              </Form>
+            </ModalBody>
+           </Modal>
         </React.Fragment>
       </div>
     );
